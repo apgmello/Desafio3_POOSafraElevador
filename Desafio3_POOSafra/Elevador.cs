0bf06d3e9a4580114b516c51a987f785f4a5ad24
Ana Mello <depaula.ana@gmail.com>
2022-06-13 18:07:25 -0300

    Add project files.

---
+++ Desafio3_POOSafra/Elevador.cs
@@ -0,0 +1,84 @@
+﻿namespace Desafio3_POOSafra
+{
+    internal class Elevador
+    {
+        private int totalAndares;
+        private int capacidade;
+        private int quantidadePessoas = 0;
+        private int andarAtual = 0;
+
+        public Elevador(int capacidade, int totalAndares)
+        {
+            if (capacidade <= 0 || totalAndares <= 0)
+            {
+                throw new ArgumentException("Quantidades devem ser positivas!");
+            }
+            else
+            {
+                this.capacidade = capacidade;
+                this.totalAndares = totalAndares;
+            }
+        }
+        public void Entrar(int pessoas)
+        {
+            if ((this.quantidadePessoas + pessoas) <= capacidade)
+            {
+                this.quantidadePessoas += pessoas;
+            }
+            else
+            {
+                throw new ArgumentException("Quantidade de pessoas excede a capacidade do elevador!");
+            }
+        }
+
+        public void Sair(int pessoas)
+        {
+            if (pessoas >= 0 && pessoas <= quantidadePessoas)
+            {
+                this.quantidadePessoas -= pessoas;
+            }
+            else
+            {
+                throw new ArgumentException("Não há pessoas para descer!");
+            }
+        }
+
+        private void Subir(int andares)
+        {
+            andarAtual += andares;
+        }
+        private void Descer(int andares)
+        {
+            andarAtual -= andares;
+        }
+
+        public void Deslocar(int andarDestino)
+        {
+            if(andarDestino > totalAndares || andarDestino < 0)
+            {
+                throw new ArgumentException("O andar solicitado não existe!");
+            }
+            
+            if(andarDestino == andarAtual)
+            {
+                throw new ArgumentException("O elevador já se encontra no andar solicitado!");
+            }
+            
+            if(andarDestino < andarAtual)
+            {
+                Descer(andarAtual - andarDestino);
+            }
+            else
+            {
+                Subir(andarDestino - andarAtual);
+            }
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"A quantidade atual de pessoas no elevador é: {quantidadePessoas}");
+            Console.WriteLine($"O andar atual é: {andarAtual}");
+        }
+    }
+
+}
---
+++ Desafio3_POOSafra/Program.cs
@@ -0,0 +1,39 @@
+﻿using Desafio3_POOSafra;
+
+int capacidade;
+int totalAndares;
+int andarDestino;
+int quantidadeDePessoas;
+int sair;
+
+try
+{
+    Console.Write("Qual a capacidade de pessoas do elevador: ");
+    int.TryParse(Console.ReadLine(), out capacidade);
+    Console.Write("Qual o total de andares do prédio: ");
+    int.TryParse(Console.ReadLine(), out totalAndares);
+
+    Elevador elevador = new Elevador(capacidade, totalAndares);
+
+    do
+    {
+        Console.Write("Informe a quantidade de pessoas que vão entrar no elevador: ");
+        int.TryParse(Console.ReadLine(), out quantidadeDePessoas);
+        elevador.Entrar(quantidadeDePessoas);
+
+        Console.Write("Qual o andar de destino: ");
+        int.TryParse(Console.ReadLine(), out andarDestino);
+        elevador.Deslocar(andarDestino);
+
+        Console.Write("Quantas pessoas vão descer: ");
+        int.TryParse(Console.ReadLine(), out sair);
+        elevador.Sair(sair);
+        Console.Clear();
+        elevador.Imprimir();
+
+    } while (true);
+}
+catch(ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
